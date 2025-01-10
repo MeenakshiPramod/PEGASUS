f7bd27d6ee04b6fac9112a013316b64e975749b5
Damian antony <jemcydami55@gmail.com>
2025-01-10 21:52:22 +0530

    style: enhance typography and layout across components with new font and CSS resets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import TextInputOutput from '../components/TextInputOutput';
 import ThemeSwitcher from "../components/ThemeSwitcher";
 import { ThemeProvider } from '../context/ThemeContext';
 import Navbar  from '../components/navbar/Navbar';
+import './App.css';
 
 
 function App() {
