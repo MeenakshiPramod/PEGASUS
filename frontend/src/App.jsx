1b1f095f7881c51a605eb3ea8d284c5b442c8fd7
Ebin746 <146419456+Ebin746@users.noreply.github.com>
2025-01-10 21:50:40 +0530

    feat: add Slider component with navigation and transition effects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,29 @@ import TextInputOutput from '../components/TextInputOutput';
 import ThemeSwitcher from "../components/ThemeSwitcher";
 import { ThemeProvider } from '../context/ThemeContext';
 import Navbar  from '../components/navbar/Navbar';
-
+import Slider from '../components/slider/Slider';
 
 function App() {
+  const contentArray = [
+    'Content 1: This is the first content.',
+    'Content 2: This is the second content.',
+    'Content 3: This is the third content.',
+    'Content 4: This is the fourth content.',
+    'Content 5: This is the fifth content.',
+    'Content 6: This is the sixth content.',
+    'Content 7: This is the seventh content.',
+    'Content 8: This is the eighth content.',
+    'Content 9: This is the ninth content.',
+    'Content 10: This is the tenth content.'
+  ];
   return (
     <ThemeProvider>
       <Navbar/>
       <ThemeSwitcher />
+
       <TextInputOutput />
+      <Slider content={contentArray}/>
+     
     </ThemeProvider>
   );
 }
